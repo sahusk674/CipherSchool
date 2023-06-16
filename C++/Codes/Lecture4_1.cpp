e12e07c156b39316062ac6d002f8b26147279210
Subrat Keshari Sahu <sahusk112@gmail.com>
2023-06-16 14:50:06 +0530

    16.6.2023

---
+++ C++/Codes/Lecture4_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int c,d;
+
+    cout << "Input 4 integers a,b,c and d:" << endl;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    cout << (a>b && c>d) << endl;
+    cout << (a>b || c>d);
+
+    return 0;
+}
