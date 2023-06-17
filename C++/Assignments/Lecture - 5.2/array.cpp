aadf0f3f0ce8f62517ed8c850dcc94cc08304f99
Subrat Keshari Sahu <sahusk112@gmail.com>
2023-06-17 14:48:33 +0530

    17.6.23

---
+++ C++/Assignments/Lecture - 5.2/array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[4];
+    int i;
+
+    cout << "Give 5 integers as input: " << endl;
+
+    for (i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "The 5 given inputs are: " << endl;
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
