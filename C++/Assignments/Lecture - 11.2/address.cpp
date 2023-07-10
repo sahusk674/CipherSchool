b8174c635fa322e1ddbcd559ccafa1ba11dbd945
Subrat Keshari Sahu <sahusk112@gmail.com>
2023-07-10 15:43:47 +0530

    10.7.23

---
+++ C++/Assignments/Lecture - 11.2/address.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    long long b;
+
+    cout << "The address of int: " << &a << endl;
+    cout << "The address of long long int: " << &b <<endl;
+
+    return 0;
+}
