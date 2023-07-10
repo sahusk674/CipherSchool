7b25af08011723b2ee0679d6a6843a9669d1f6b0
Subrat Keshari Sahu <sahusk112@gmail.com>
2023-07-10 14:26:16 +0530

    10.7.23

---
+++ C++/Codes/memory.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    long long b;
+
+    cout << "The size of int: " << sizeof(a) << endl;
+    cout << "The size of long long int: " << sizeof(b) <<endl;
+
+    return 0;
+}
